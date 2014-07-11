41a929c1724d607983e9ef886dd235cbd201556e
Julia <juliajtan@gmail.com>
2014-07-11 18:07:35 +0100

    Fixed a bug with regards to the querying of tags in the database

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Post' do
 
-  let(:post) { Post.create }
+  let(:post) { Post.create title: 'post', description: 'desc'}
 
   # place these tags here since tags are created and displayed on posts
   describe '#tag names=' do
@@ -16,14 +16,14 @@ describe 'Post' do
     describe 'one tag that does not exist' do
       it 'adds a tag to the post' do
         post.tag_names = '#yolo'
-        expect(post.tags.length).to eq 1
+        expect(post.tags.count).to eq 1
       end
     end
 
     describe 'two tags that do not exist' do
       it 'adds both tags to the post' do
         post.tag_names = '#yolo, #swag'
-        expect(post.tags.length).to eq 2
+        expect(post.tags.count).to eq 2
       end
     end
 
@@ -31,7 +31,7 @@ describe 'Post' do
       context 'without spaces' do
         it 'adds each tag to the post' do
           post.tag_names = 'yolo,swag'
-          expect(post.tags.length).to eq 2
+          expect(post.tags.count).to eq 2
         end
       end
     end
