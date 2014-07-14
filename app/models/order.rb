cef46a18c717d196a56f1a222ee116edf602f4bd
Julia <juliajtan@gmail.com>
2014-07-14 13:06:26 +0100

    Displays orders if there are orders and you are logged in as admin

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,11 @@
 class Order < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
+
+  def number
+      date_section = created_at.strftime('%d%m%y')
+      number_section = "%04d" % id
+
+      date_section + number_section
+  end
 end
