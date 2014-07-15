0435757dc66a1e377fc0b7787c135680d46af656
Julia <juliajtan@gmail.com>
2014-07-15 17:06:12 +0100

    Add sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,12 +82,12 @@ Rails.application.configure do
 
   # Code from SendGrid to enable emailing on Heroku
   ActionMailer::Base.smtp_settings = {
-  :user_name => 'your_sendgrid_username',
-  :password => 'your_sendgrid_password',
-  :domain => 'yourdomain.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 end
