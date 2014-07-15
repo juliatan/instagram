c899fd342782f9d5be8bf8a99d30fc771b010bc0
Julia <juliajtan@gmail.com>
2014-07-15 13:41:39 +0100

    Can send email confirmation after an order is made

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,7 @@ class OrderMailer < ActionMailer::Base
   default from: "no-reply@instacat.com"
 
   def confirmation(order)
-    mail(to: order.user.email, subject: "You've successfully ordered a Pretty picture")
+    @order = order
+    mail(to: order.user.email, subject: "You've successfully ordered a #{order.post.title}")
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,10 +2,16 @@ class Order < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
 
+  after_create :send_confirmation_email
+
   def number
       date_section = created_at.strftime('%d%m%y')
       number_section = "%04d" % id # alias for sprintf == "%04d".%(id)
 
       date_section + number_section
+  end
+
+  def send_confirmation_email
+    OrderMailer.confirmation(self).deliver
   end
 end
