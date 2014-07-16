d38918c8e29e21575f7e64d9a2047eb6cf65a884
Julia <juliajtan@gmail.com>
2014-07-16 13:25:04 +0100

    Likes counter increments when clicked

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -5,9 +5,15 @@ describe 'liking posts' do
     julia = User.create(name: 'Julia', email: 'test@test.com', password: '12345678', password_confirmation: '12345678')
     julia.posts.create(title: 'Test post', description: 'Test description')
   end
-  
+
   it 'is initially at 0 likes' do
     visit '/posts'
     expect(page).to have_link "❤ 0"
+  end
+
+  it 'can be liked, incrementing the likes count' do
+    visit '/posts'
+    click_link '❤ 0'
+    expect(page).to have_link '❤ 1'
   end
 end
